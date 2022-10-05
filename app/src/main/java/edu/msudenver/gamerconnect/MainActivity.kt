378df749ab55c86814f288840cd3f0be3797b680
Dende <denverg52@hotmail.com>
2022-10-05 14:50:18 -0600

    -removed <<<<head in activity_login -removed bundle from ActivityMain

--- app/src/main/java/edu/msudenver/gamerconnect/MainActivity.kt
+++ app/src/main/java/edu/msudenver/gamerconnect/MainActivity.kt
@@ -3,8 +3,6 @@ package edu.msudenver.gamerconnect
 import android.os.Bundle
 
 class MainActivity {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+
 }
