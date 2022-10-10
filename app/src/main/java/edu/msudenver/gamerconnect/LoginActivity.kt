d5b874f24d83df93c1f50c28e08de758386d5ae1
Dende <denverg52@hotmail.com>
2022-10-10 17:24:37 -0600

    -add rsc id to activity_login.xml -LoginActivity variables

--- app/src/main/java/edu/msudenver/gamerconnect/LoginActivity.kt
+++ app/src/main/java/edu/msudenver/gamerconnect/LoginActivity.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lateinit var loginBtn: Button
+        lateinit var email: EditText
+        lateinit var password: EditText
+
+        loginBtn = findViewById(R.id.loginBtn)
+        email = findViewById(R.id.userEmail)
+        password = findViewById(R.id.userPassword)
+
+        loginBtn.setOnClickListener{
+
+        }
 
         val buttonClick = findViewById<Button>(R.id.createButton)
         buttonClick.setOnClickListener {
