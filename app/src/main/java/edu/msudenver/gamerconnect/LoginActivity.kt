1d07e432ac3c2893c4a9c94a37bd058e0e54fee8
DendeLime <DenverG52@hotmail.com>
2022-10-10 22:04:05 -0600

    LoginActivity.kt add to button click and variables

--- app/src/main/java/edu/msudenver/gamerconnect/LoginActivity.kt
+++ app/src/main/java/edu/msudenver/gamerconnect/LoginActivity.kt
@@ -5,22 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import edu.msudenver.gamerconnect.manageaccount.AccountAuth
 import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         lateinit var loginBtn: Button
-        lateinit var email: EditText
-        lateinit var password: EditText
+        lateinit var emailEdtTxt: EditText
+        lateinit var passwordEdtTxt: EditText
+        lateinit var email:String
+        lateinit var password:String
 
         loginBtn = findViewById(R.id.loginBtn)
-        email = findViewById(R.id.userEmail)
-        password = findViewById(R.id.userPassword)
+        emailEdtTxt = findViewById(R.id.userEmail)
+        passwordEdtTxt = findViewById(R.id.userPassword)
+
 
         loginBtn.setOnClickListener{
+            email = emailEdtTxt.text.toString()
+            password = passwordEdtTxt.text.toString()
+
+            if (email.contains("@")) {
+                Toast.makeText(applicationContext,
+                    "Please enter a valid email",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
 
         }
 
