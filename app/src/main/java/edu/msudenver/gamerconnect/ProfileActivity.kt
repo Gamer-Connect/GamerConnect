d1e1ba462a129dd48ea38524a43f2d5fe4739ded
DendeLime <denverg52@mail.com>
2022-11-02 16:10:57 -0600

    Create working profile branch

--- app/src/main/java/edu/msudenver/gamerconnect/ProfileActivity.kt
+++ app/src/main/java/edu/msudenver/gamerconnect/ProfileActivity.kt
@@ -4,11 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+
+
+
+        fun getCurrentUser() {
+            val user = Firebase.auth.currentUser
+            user?.let {
+                val name = user.displayName
+                val email = user.email
+                val photoUrl = user.photoUrl
+
+                val emailVerified = user.isEmailVerified
+
+                // Unique Firebase user ID
+                // Use FirebaseUser.getToken() to confirm with Firebase
+                val userId = user.uid
+            }
+
+        }
+
 
         val logoutButton = findViewById<Button>(R.id.logoutBtn)
         logoutButton.setOnClickListener {
